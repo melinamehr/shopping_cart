54ccef26e02a52bd46c28cde3bfc84cc955a1761
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-05-23 15:34:12 -0400

    working on sums

--- cart.rb
+++ cart.rb
@@ -1,1 +1,43 @@
-class Shopping_cart
+require_relative 'product.rb'
+
+class Shopping_carts
+  @@products = []
+
+  def self.all_products
+    @@products
+  end
+
+  def initialize
+    @total_before = 0
+    @total_after = 0
+  end
+
+  def self.add_product(name, price)
+    new_product = Products.new(name, price)
+    @@products << new_product
+    new_product
+  end
+
+  def self.rem_product(product)
+    @@products.delete(product)
+  end
+
+
+  def self.total_before
+    @total_before=0
+    @@products.each do |product|
+      @total_before += product.price
+    end
+    @total_before
+  end
+
+end
+
+
+  #Each shopping cart has a collection of products.
+  #It should also have the following functionality:
+
+  #add an product to the cart
+  #remove an product from the cart
+  #add up the total cost of all products in the cart before tax
+  #add up the total cost of all products in the cart after tax
--- product.rb
+++ product.rb
@@ -1,5 +1,6 @@
 class Products
-  attr_accessor :name, :price, :tax
+  attr_accessor :name, :price
+  attr_reader :tax
 
  def initialize(name, price)
   @name = name
@@ -15,11 +16,3 @@ end
   product2 = Products.new("donut", 7)
   product3 = Products.new("tea", 3)
 end
-
-  #Each shopping cart has a collection of products.
-  #It should also have the following functionality:
-
-  #add an product to the cart
-  #remove an product from the cart
-  #add up the total cost of all products in the cart before tax
-  #add up the total cost of all products in the cart after tax
